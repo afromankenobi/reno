44aeeb48022bbeb10c64d3ac14ca01ac8cb6f963
Jorge Vargas Silva <jv.vargass@gmail.com>
2011-11-26 14:47:15 -0300

    Generacion de proyecto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reno::Application.config.secret_token = 'c26fa5d358630e5da8f9b228feae192ddac57cc23c21a8eca73a947575e5110217565fa59995a5dd45fd2be986ae67460197978a9346f6f03d93be324a1ea17a'
